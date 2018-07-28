b45c0e45098838373066401c207dc29316fa9296
Andromeda <eclipse0010101@gmail.com>
2018-07-28 16:36:11 -0500

    Add files via upload

---
+++ REhelper.cpp
@@ -0,0 +1,20 @@
+// Copyright 2018 <Andromeda>
+#include <../headers/options.h>
+
+
+int main(int argc, char **argv) {
+    REhelp options;
+
+    switch (argc) {
+        case 2:
+            options.pick1(argv[1]);
+        case 3:
+            options.pick2(argv[1], argv[2]);
+        case 4:
+            options.pick3(argv[1], argv[2], argv[3]);
+        default:
+            options.usage();
+        }
+
+    return 0;
+}
